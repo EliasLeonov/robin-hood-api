896c2dc334cb75ff1fc23b7c9d1dfe934ff67ac3
Elias <eliasleonov99@gmail.com>
2021-06-01 18:07:50 -0300

    first commit

---
+++ src/main/java/aseca/roobinhood/api/RobinHoodApiApplication.java
@@ -0,0 +1,13 @@
+package aseca.roobinhood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinHoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinHoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aseca/roobinhood/api/RobinHoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aseca.roobinhood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinHoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
