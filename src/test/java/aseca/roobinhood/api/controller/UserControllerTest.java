970e7bbc824c4dd710818d6135b3329843c5aab6
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-01 23:52:56 -0300

    added mem profile

--- src/test/java/aseca/roobinhood/api/controller/UserControllerTest.java
+++ src/test/java/aseca/roobinhood/api/controller/UserControllerTest.java
@@ -1,13 +1,21 @@
 package aseca.roobinhood.api.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(SpringRunner.class)
+@ActiveProfiles("mem")
+@SpringBootTest
 class UserControllerTest {
 
     @Test
     public void test_001_create_company(){
+        System.out.println();
+        Assertions.assertThat(true).isTrue();
     }
 
 }
