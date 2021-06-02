658c4d08a4e510b344a8d7b2834af266f734504f
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-02 00:25:34 -0300

    delete stock transactions

--- src/main/java/aseca/roobinhood/api/domain/Stock.java
+++ src/main/java/aseca/roobinhood/api/domain/Stock.java
@@ -1,24 +1,20 @@
 package aseca.roobinhood.api.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.HashSet;
-import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 @Data
 @Builder
 @Entity
 @Table(name = "stock")
 public class Stock extends AbstractEntity {
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "stock")
-    private Set<Transaction> transactions = new HashSet<>();
 
 }
