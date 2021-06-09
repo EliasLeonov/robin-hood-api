702730aa47888f182f87a48adcd02f61efbfe6e5
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-06-08 21:17:26 -0300

    added get all by stock

--- src/main/java/aseca/roobinhood/api/controller/TransactionController.java
+++ src/main/java/aseca/roobinhood/api/controller/TransactionController.java
@@ -5,13 +5,15 @@ import aseca.roobinhood.api.dto.TransactionDto;
 import aseca.roobinhood.api.dto.TransactionResponseDto;
 import aseca.roobinhood.api.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transaction")
 public class TransactionController {
 
     private final TransactionService transactionService;
@@ -21,12 +23,12 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    @PostMapping
+    @PostMapping("/transaction")
     public TransactionResponseDto buy(@RequestBody @Valid TransactionDto transactionDto){
         return transactionService.buyStock(transactionDto);
     }
 
-    @GetMapping
+    @GetMapping("/get-all")
     public List<StockDto> getAllByUser(){
         return transactionService.getAllStocks();
     }
